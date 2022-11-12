06198c9de27003b5fdfa821e7e1a32f203094abd
kewenlok <kewen.lok@u.nus.edu>
2022-11-12 10:59:02 +0800

    Update automation script

--- k8s/manifests/k8s/a2_setup.sh
+++ k8s/manifests/k8s/a2_setup.sh
@@ -1,5 +1,5 @@
-kind create cluster --name kind-1 --config k8s/kind/cluster-config.yaml
-kubectl apply -f k8s/manifests/k8s/backend-deployment.yaml
-kubectl apply -f k8s/manifests/k8s/backend-service.yaml
+kind create cluster --name kind-1 --config ../../kind/cluster-config.yaml
+kubectl apply -f backend-deployment.yaml
+kubectl apply -f backend-service.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-kubectl apply -f k8s/manifests/k8s/backend-ingress.yaml
+kubectl apply -f backend-ingress.yaml
